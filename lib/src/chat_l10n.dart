159c85ffe1275a2cadc321f29c2d854d677e5f07
Wei-Hsun, Huang <F107152155@nkust.edu.tw>
2021-11-22 00:37:01 +0800

    Add Traditional Chinese L10n (#145)
    * add Traditional Chinese L10n

--- lib/src/chat_l10n.dart
+++ lib/src/chat_l10n.dart
@@ -226,3 +226,25 @@ class ChatL10nZhCN extends ChatL10n {
           sendButtonAccessibilityLabel: sendButtonAccessibilityLabel,
         );
 }
+
+/// Traditional Chinese l10n which extends [ChatL10n]
+@immutable
+class ChatL10nZhTW extends ChatL10n {
+  /// Creates Traditional Chinese l10n. Use this constructor if you want to
+  /// override only a couple of properties, otherwise create a new class
+  /// which extends [ChatL10n]
+  const ChatL10nZhTW({
+    String attachmentButtonAccessibilityLabel = '傳送多媒體',
+    String emptyChatPlaceholder = '尚無訊息',
+    String fileButtonAccessibilityLabel = '檔案',
+    String inputPlaceholder = '請輸入訊息...',
+    String sendButtonAccessibilityLabel = '傳送',
+  }) : super(
+          attachmentButtonAccessibilityLabel:
+              attachmentButtonAccessibilityLabel,
+          emptyChatPlaceholder: emptyChatPlaceholder,
+          fileButtonAccessibilityLabel: fileButtonAccessibilityLabel,
+          inputPlaceholder: inputPlaceholder,
+          sendButtonAccessibilityLabel: sendButtonAccessibilityLabel,
+        );
+}
