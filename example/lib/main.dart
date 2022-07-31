a2f0395c4619af0a72d196846c68d9be07af35aa
otto-dev <ignatius.public@gmail.com>
2022-08-01 08:57:45 +1000

    revert example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
@@ -36,28 +37,11 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   List<types.Message> _messages = [];
   final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
-  late Timer timer;
-  int counter = 0;
 
   @override
   void initState() {
-    timer = Timer.periodic(const Duration(seconds: 5), (_) {
-      final textMessage = types.CustomMessage(
-        author: _user,
-        metadata: {"index": counter++},
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: const Uuid().v4(),
-      );
-      _addMessage(textMessage);
-    });
     super.initState();
     _loadMessages();
-  }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
   }
 
   @override
@@ -71,13 +55,6 @@ class _ChatPageState extends State<ChatPage> {
           showUserAvatars: true,
           showUserNames: true,
           user: _user,
-          customMessageBuilder: (message, {required int messageWidth}) =>
-              _CustomMessage(
-            index: message.metadata!['index'],
-            onTap: () => setState(
-              () => _messages.removeWhere((element) => element == message),
-            ),
-          ),
         ),
       );
 
@@ -247,64 +224,13 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   void _loadMessages() async {
-    // final response = await rootBundle.loadString('assets/messages.json');
-    // final messages = (jsonDecode(response) as List)
-    //     .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-    //     .toList();
-    //
-    // setState(() {
-    //   _messages = messages;
-    // });
-  }
-}
+    final response = await rootBundle.loadString('assets/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
+        .toList();
 
-class _CustomMessage extends StatefulWidget {
-  final int index;
-  final void Function() onTap;
-
-  const _CustomMessage({Key? key, required this.index, required this.onTap})
-      : super(key: key);
-
-  @override
-  _CustomMessageState createState() => _CustomMessageState();
-}
-
-class _CustomMessageState extends State<_CustomMessage>
-    with AutomaticKeepAliveClientMixin {
-  late Timer timer;
-  int counter = 0;
-
-  @override
-  void initState() {
-    timer = Timer.periodic(const Duration(milliseconds: 300), (_) {
-      setState(() {
-        counter++;
-      });
+    setState(() {
+      _messages = messages;
     });
-    super.initState();
   }
-
-  @override
-  void dispose() {
-    timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      onTap: widget.onTap,
-      child: Container(
-        padding: const EdgeInsets.all(8),
-        color: Colors.blue,
-        child: Text(
-          '#${widget.index}${wantKeepAlive ? ' keepAlive' : ''} - Counter: $counter',
-          style: Theme.of(context).textTheme.headline6,
-        ),
-      ),
-    );
-  }
-
-  @override
-  bool get wantKeepAlive => widget.index % 2 == 0;
 }
