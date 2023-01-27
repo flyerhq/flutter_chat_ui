3739dadf16c234b264570fa0f64ca0c0937e109b
leeyi <liyuanbing@yongdaxing.cn>
2023-01-27 23:59:04 +0800

    format

--- lib/src/widgets/chat.dart
+++ lib/src/widgets/chat.dart
@@ -28,7 +28,6 @@ import 'state/inherited_user.dart';
 import 'typing_indicator.dart';
 import 'unread_header.dart';
 
-
 /// Keep track of all the auto scroll indices by their respective message's id to allow animating to them.
 final Map<String, int> chatMessageAutoScrollIndexById = {};
 
