b5e1d6ddc79b3d57163870c1fe13a381d504d708
okano <okano4413@gmail.com>
2023-10-30 10:55:58 +0900

    Fix #476

--- lib/src/widgets/input/input.dart
+++ lib/src/widgets/input/input.dart
@@ -107,6 +107,9 @@ class _InputState extends State<Input> {
   }
 
   void _handleTextControllerChange() {
+    if (_textController.value.isComposingRangeValid) {
+      return;
+    }
     setState(() {
       _sendButtonVisible = _textController.text.trim() != '';
     });
