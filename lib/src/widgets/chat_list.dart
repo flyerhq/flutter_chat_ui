fa0979287e7e5a3945e18f94ecaf44e8a16f9cee
emresaridogan <yesaridogan@gmail.com>
2023-12-01 22:34:22 +0300

    Refactor _newMessageBuilder method in ChatList

--- lib/src/widgets/chat_list.dart
+++ lib/src/widgets/chat_list.dart
@@ -135,12 +135,7 @@ class _ChatListState extends State<ChatList>
   Widget _newMessageBuilder(int index, Animation<double> animation) {
     try {
       final item = _oldData[index];
-      return SizeTransition(
-        key: _valueKeyForItem(item),
-        axisAlignment: -1,
-        sizeFactor: animation.drive(CurveTween(curve: Curves.easeOutQuad)),
-        child: widget.itemBuilder(item, index),
-      );
+      return widget.itemBuilder(item, index);
     } catch (e) {
       return const SizedBox();
     }
