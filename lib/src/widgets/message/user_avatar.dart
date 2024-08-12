0bbd453662022d263366d3b85ad0995c468513d9
Alex Demchenko <alexdemchenko@yahoo.com>
2024-08-12 08:11:36 +0200

    pipeline fixes

--- lib/src/widgets/message/user_avatar.dart
+++ lib/src/widgets/message/user_avatar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
+import '../../conditional/conditional.dart';
 import '../../models/bubble_rtl_alignment.dart';
 import '../../util.dart';
 import '../state/inherited_chat_theme.dart';
-import '../../conditional/conditional.dart';
 
 /// Renders user's avatar or initials next to a message.
 class UserAvatar extends StatelessWidget {
