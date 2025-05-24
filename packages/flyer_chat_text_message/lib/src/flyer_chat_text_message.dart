aa73f33c6cddb798493b3929fd82e0120868d0f3
Nicolas Braun <braunico@gmail.com>
2025-05-24 08:51:21 +0200

    Fix forgotten extra linkPreview widget on top

--- packages/flyer_chat_text_message/lib/src/flyer_chat_text_message.dart
+++ packages/flyer_chat_text_message/lib/src/flyer_chat_text_message.dart
@@ -151,9 +151,6 @@ class FlyerChatTextMessage extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (linkPreviewWidget != null &&
-                linkPreviewPosition == LinkPreviewPosition.top)
-              linkPreviewWidget,
             Container(
               padding:
                   _isOnlyEmoji
